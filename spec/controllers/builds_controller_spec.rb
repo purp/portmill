00eff5c42baebbd3c4f1136f6dbfe0e2d5feff88
Jim Meyer <jim@geekdaily.org>
2009-06-30 18:07:38 -0700

    Massive refactor: added mock_build method, reordered tests to put REST calls at top, consolidated many test cases for speed and readability, fixed broken spec for feed content type, added specs to reach 100% coverage

--- spec/controllers/builds_controller_spec.rb
+++ spec/controllers/builds_controller_spec.rb
@@ -1,84 +1,96 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe BuildsController do
+
+  def mock_build(stubs={})
+    @mock_build ||= mock_model(Build, stubs)
+  end
+
   describe "should index" do
     before(:each) do
-      Build.stub!(:paginate).and_return(@mock_builds = mock_model(Array))
+      Build.stub!(:paginate).and_return(@mock_builds = [mock_build])
     end
 
-    it "successfully" do
+    it "successfully and assign builds" do
       get :index
       response.should be_success
-    end
-
-    it "assign @builds" do
-      get :index
       assigns(:builds).should == @mock_builds
     end
   end
 
-  describe "handles CouchDB connection refused exception" do
-    describe "on /show" do
-      before do
-        Build.stub!(:find).and_raise(Errno::ECONNREFUSED)
-      end
-      
-      it "renders an error message" do
-        get :show, :id => "35340"
-        response.should be_success
-        response.should have_text("Database: connection refused")
-      end
+  describe "should show" do
+    it "GET build" do
+      Build.should_receive(:find).with("35340").and_return(mock_build)
+      get :show, :id => "35340"
+      response.should be_success
+      assigns[:build].should equal(mock_build)
     end
 
-    describe "on /create" do
-      before do
-        JSON.stub!(:parse)
-        Build.stub!(:json_create).and_raise(Errno::ECONNREFUSED)
-      end
-      
-      it "renders an error message" do
-        put :create, :post => { :body => nil }
-        response.should be_success
-        response.should have_text("Database: connection refused")
-      end
+    it "GET build without id" do
+      get :show
+      response.should redirect_to(:action => :index)
     end
   end
 
-  describe "should ping" do
-    def do_ping
-      post :ping
+  describe "should create" do
+    before(:each) do
+      @build_json = '{"log":null,"created_at":"1999/06/19 23:00:00 +0000","revision":61999,"state":"success","updated_at":"1999/06/19 23:00:00 +0000","cpu":"i686","time":"1999/06/19 23:00:00 +0000","name":"foo","os":"Darwin 9.7.0","ruby_class":"Build"}'
+      @mock_build = mock_build(:to_json => @build_json, :save! => true)
+      JSON.stub!(:parse)
+      Build.stub!(:json_create).and_return(@mock_build)
     end
 
-    it "successfully" do
-      do_ping
+    it "POST create" do
+      put :create, :post => { :body => nil }
       response.should be_success
+      response.headers["Content-Type"].should match(/^application\/json/)
+      response.body.should == @build_json
     end
-
-    it "and respond 'Ok'" do
-      do_ping
+  end
+  
+  describe "should ping" do
+    it "successfully and respond Ok" do
+      post :ping
+      response.should be_success
       response.should have_text("Ok")
     end
   end
 
   describe "feed action" do
-    before do
+    before(:each) do
       builds = []
       10.times do
-        builds << mock_model(Build)
+        builds << mock_build
       end
       Build.should_receive(:paginate).with(1,10,an_instance_of(Hash)).and_return(builds)
     end
 
-    it "create response" do
+    it "create response with proper content type" do
       get :feed
       response.should be_success
+      response.headers["Content-type"].should_not be_nil
+      response.headers["Content-type"].should match(/application\/rss\+xml/)
       assigns[:builds].should_not be_nil
-    end
-
-    it "set content type" do
-      get :feed
-      response.headers["type"].should match(/application\/rss\+xml/)
     end
   end
 
+  describe "handles CouchDB connection refused exception" do
+    before(:each) do
+      JSON.stub!(:parse)
+      Build.stub!(:find).and_raise(Errno::ECONNREFUSED)
+      Build.stub!(:json_create).and_raise(Errno::ECONNREFUSED)
+    end
+
+    it "succeeds and renders an error message on show" do
+      get :show, :id => "35340"
+      response.should be_success
+      response.should have_text("Database: connection refused")
+    end
+
+    it "succeeds and renders an error message on create" do
+      put :create, :post => { :body => nil }
+      response.should be_success
+      response.should have_text("Database: connection refused")
+    end
+  end
 end
